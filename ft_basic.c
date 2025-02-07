00aca0a75b58b3898bd41ffe9036bcdb54fbf549
Leireay <163833943+Leireay@users.noreply.github.com>
2025-02-07 11:06:32 +0100

    Update ft_basic.c

--- ft_basic.c
+++ ft_basic.c
@@ -4,15 +4,11 @@ int	ft_print_string(char *str)
 {
 	int	size;
 
+	if (!str)
+		return (ft_print_string("null"));
 	size = 0;
-	if (str == 0)
-	{
-		return (ft_print_string("(null)"));
-	}
 	while (str[size])
-	{
 		size += ft_print_character(str[size]);
-	}
 	return (size);
 }
 
@@ -21,20 +17,14 @@ int	ft_print_number(int n)
 	int	size;
 
 	size = 0;
-	if (n == 0)
-		size += ft_print_character('0');
 	if (n == -2147483648)
-	{
-		size += ft_print_string("-2147483648");
-		return (size);
-	}
+		return (ft_print_string("-2147483648"));
 	if (n < 0)
 	{
 		size += ft_print_character('-');
 		n = -n;
 	}
-	if (n > 0)
-		size += ft_print_unsigned((unsigned int)n);
+	size += ft_print_unsigned((unsigned int)n);
 	return (size);
 }
 
@@ -43,18 +33,8 @@ int	ft_print_unsigned(unsigned int n)
 	int	size;
 
 	size = 0;
-	if (n == 0)
-		size += ft_print_character('0');
-	else
-	{
-		if (n / 10 != 0)
-			ft_print_unsigned(n / 10);
-		ft_print_character((n % 10) + '0');
-		while (n > 0)
-		{
-			n /= 10;
-			size++;
-		}
-	}
+	if (n >= 10)
+		size += ft_print_unsigned(n / 10);
+	size += ft_print_character((n % 10) + '0');
 	return (size);
 }
