0e2e03c31e6e08940126aa45a66ba91316495db2
Leireay <163833943+Leireay@users.noreply.github.com>
2025-02-07 11:05:06 +0100

    Update ft_hexadecimals.c

--- ft_hexadecimals.c
+++ ft_hexadecimals.c
@@ -1,48 +1,45 @@
 #include "ft_printf.h"
 
 static int	ft_length_hexadecimal(unsigned int num);
+static int	ft_search_hexadecimal(unsigned int num, char word);
 
-static void	ft_search_hexadecimal(unsigned int num, const char word);
-
-int	ft_print_hexadecimal(unsigned int num, const char word)
+int	ft_print_hexadecimal(unsigned int num, char word)
 {
 	if (num == 0)
 		return (ft_print_character('0'));
-	else
-		ft_search_hexadecimal(num, word);
-	return (ft_length_hexadecimal(num));
+	return (ft_search_hexadecimal(num, word));
 }
 
-static void	ft_search_hexadecimal(unsigned int num, const char word)
+static int	ft_search_hexadecimal(unsigned int num, char word)
 {
+	int	size;
+
+	size = 0;
 	if (num >= 16)
-	{
-		ft_search_hexadecimal(num / 16, word);
-		ft_search_hexadecimal(num % 16, word);
-	}
+		size += ft_search_hexadecimal(num / 16, word);
+	if (num % 16 < 10)
+		size += ft_print_character((num % 16) + '0');
 	else
 	{
-		if (num < 10)
-			ft_print_character(num + '0');
-		else
-		{
-			if (word == 'x')
-				ft_print_character(num - 10 + 'a');
-			if (word == 'X')
-				ft_print_character(num - 10 + 'A');
-		}
+		if (word == 'x')
+			size += ft_print_character((num % 16) - 10 + 'a');
+		else if (word == 'X')
+			size += ft_print_character((num % 16) - 10 + 'A');
 	}
+	return (size);
 }
 
 static int	ft_length_hexadecimal(unsigned int num)
 {
 	int	len;
 
+	if (num == 0)
+		return (1);
 	len = 0;
 	while (num != 0)
 	{
 		len++;
-		num = num / 16;
+		num /= 16;
 	}
 	return (len);
 }
