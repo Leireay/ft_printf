87599bac15d84817f4e11bc0957899487f962899
Leireay <163833943+Leireay@users.noreply.github.com>
2025-02-08 10:29:46 +0100

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -1,1 +1,14 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
+# include <unistd.h>
+# include <stdarg.h>
+# include <sys/types.h>
+
+int		ft_printf(const char *str, ...);
+size_t	ft_putnbr_base(ssize_t nbr, char *base, size_t len, char spf);
+size_t	ft_putstr(const char *str);
+size_t	ft_putchar(const char c);
+char	*ft_strchr(const char *s, int c);
+
+#endif  /* FT_PRINTF_H */
