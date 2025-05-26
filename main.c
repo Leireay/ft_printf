99eb024779f946097ff813887dadd770b46f8c10
Leireay <163833943+Leireay@users.noreply.github.com>
2025-05-26 12:01:46 +0200

    Update main.c
    Añadido un main

--- main.c
+++ main.c
@@ -10,3 +10,17 @@ int main()
 	printf("%d\n %i\n %u\n %c\n %x\n %X\n %p\n %s\n", 17, 1908, -2311, 'x', 11, 11, i, str);
 	return (0);
 }
+/*
+MAIN DE ft_printf.c
+int	main(void)
+{
+	int	valor;
+	int	*puntero;
+
+	valor = 45;
+	puntero = &valor;
+	ft_printf("%p", (void *)puntero);
+	printf("\n\n%p", (void *)puntero);
+	return (0);
+}
+*/
